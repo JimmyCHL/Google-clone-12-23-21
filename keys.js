034b76aa4c19c2f046e9b1f0b4fd4a65ca832a93
Jimmy Lo <83715466+JimmyCHL@users.noreply.github.com>
2021-12-23 01:36:35 -0500

    untrack key

--- keys.js
+++
@@ -1,9 +0,0 @@
-// Google Search API Key - https://developers.google.com/customsearch/v1/using_rest
-// An API key is a way to identify your client to Google.
-// Only Hundred request a day
-export const API_KEY = "AIzaSyCv8jVFNT4ng8xVMqKLqFB2vouM7WRV84o";
-
-// Context Key - https://cse.google.com/cse/create/new
-// create search engine
-//Programmable Search Engine ID - Use cx to specify the Programmable Search Engine you want to use to perform this search.
-export const CONTEXT_kEY = "5d66f590ce376c925";
